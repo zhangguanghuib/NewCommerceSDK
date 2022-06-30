d38d0be514fcadfffbb5a5384fc4e0c174ff6cb6
zhangguanghuib <=>
2022-06-30 14:26:55 +0800

    Remove MPOS and add powershell script

---
+++ StoreHoursSample/Scripts/Uninstall-Base-Product.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+Uninstalls the Commerce Scale Unit.
+#>
+Import-Module (Join-Path $PSScriptRoot "ErrorDecorator.psm1")
+
+$workspaceFolder = $Env:common_workspaceFolder
+
+Write-Host
+$InstallerPath = Join-Path $workspaceFolder "Download\CommerceStoreScaleUnitSetup.exe"
+if (Test-Path -Path $InstallerPath) {
+    Write-Host "Uninstalling the base product."
+    & "$InstallerPath" uninstall
+    if ($LastExitCode -ne 0) {
+        Write-Host
+        Write-CustomError "The base product uninstallation has failed with exit code $LastExitCode. Please examine the above logs to fix a problem and start again."
+        Write-Host
+        exit $LastExitCode
+    }
+}
+else {
+    Write-Host "The base product installer was not found in "$workspaceFolder\Download\" directory."
+}
+
