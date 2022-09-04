1004da7c2b02f4ef4a2ebb764b4e15ade91fc9cb
zhangguanghuib <=>
2022-09-04 21:36:18 +0800

    Add BarcodeMsrDialog html/ts

---
+++ BarcodeMsrDialogSample/Pos/BarcodeMsrDialog.ts
@@ -0,0 +1,18 @@
+﻿import { ExtensionTemplatedDialogBase, ITemplatedDialogOptions } from "PosApi/Create/Dialogs";
+import { ObjectExtensions } from "PosApi/TypeExtensions";
+import { ClientEntities } from "PosApi/Entities";
+import { IAlphanumericNumPad, IAlphanumericNumPadOptions } from "PosApi/Consume/Controls";
+import { BarcodeMsrDialogInputType, IBarcodeMsrDialogResult } from "./BarcodeMsrDialogTypes";
+
+type BarcodeMsrDialogResolveFunction = (value: IBarcodeMsrDialogResult) => void;
+type BarcodeMsrDialogREjectFunction = (reason: any) => void;
+
+export default class BarcodeMsrDialog extends ExtensionTemplatedDialogBase {
+
+    public numPad: IAlphanumericNumPad;
+
+    onReady(element: HTMLElement): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
---
+++ BarcodeMsrDialogSample/Pos/BarcodeMsrDialogTypes.ts
@@ -0,0 +1,7 @@
+﻿export type BarcodeMsrDialogInputType = "None" | "Manual" | "Barcode" | "MSR";
+
+export interface IBarcodeMsrDialogResult {
+    canceled: boolean;
+    inputType?: BarcodeMsrDialogInputType;
+    value?: string;
+}
