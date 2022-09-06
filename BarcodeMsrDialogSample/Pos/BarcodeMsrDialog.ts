49a21f844ba27d2e9cb70db3b24e07673e5ac01f
zhangguanghuib <=>
2022-09-06 18:51:28 +0800

    Add FulfillmentLineCommand

--- BarcodeMsrDialogSample/Pos/BarcodeMsrDialog.ts
+++ BarcodeMsrDialogSample/Pos/BarcodeMsrDialog.ts
@@ -5,14 +5,83 @@ import { IAlphanumericNumPad, IAlphanumericNumPadOptions } from "PosApi/Consume/
 import { BarcodeMsrDialogInputType, IBarcodeMsrDialogResult } from "./BarcodeMsrDialogTypes";
 
 type BarcodeMsrDialogResolveFunction = (value: IBarcodeMsrDialogResult) => void;
-type BarcodeMsrDialogREjectFunction = (reason: any) => void;
+type BarcodeMsrDialogRejectFunction = (reason: any) => void;
 
 export default class BarcodeMsrDialog extends ExtensionTemplatedDialogBase {
 
     public numPad: IAlphanumericNumPad;
 
-    onReady(element: HTMLElement): void {
-        throw new Error("Method not implemented.");
+    private resolve: BarcodeMsrDialogResolveFunction;
+    private _inputType: BarcodeMsrDialogInputType;
+    private _automateEntryInProgress: boolean;
+
+    constructor() {
+        super();
+
+        this._inputType = "None";
+        this._automateEntryInProgress = false;
+
+        this.onBarcodeScanned = (data: string): void => {
+            this._automateEntryInProgress = true;
+            this.numPad.value = data;
+            this._inputType = "Barcode";
+            this._automateEntryInProgress = false;
+        };
+
+        this.onMsrSwiped = (data: ClientEntities.ICardInfo): void => {
+            this._automateEntryInProgress = true;
+            this.numPad.value = data.CardNumber;
+            this._inputType = "MSR";
+            this._automateEntryInProgress = false;
+        }
+    }
+
+    public onReady(element: HTMLElement): void {
+        let numPadOptions: IAlphanumericNumPadOptions = {
+            globalInputBroker: this.numPadInputBroker,
+            label: "Please enter a value, scan or swipe",
+            value: ""
+        };
+
+        let numPadRootElem: HTMLDivElement = element.querySelector("#barcodeMsrDialogAlphanumbericNumpad") as HTMLDivElement;
+        this.numPad = this.context.controlFactory.create(this.context.logger.getNewCorrelationId(), "AlphanumericNumPad", numPadOptions, numPadRootElem);
+        this.numPad.addEventListener("EnterPressed", (eventData: { value: Commerce.Extensibility.NumPadValue }) => {
+            this._resolvePromise({ canceled: false, inputType: this._inputType, value: eventData.value.toString() });
+        });
+
+        this.numPad.addEventListener("ValueChanged", (eventData: { value: string }) => {
+            if (!this._automateEntryInProgress) {
+                this._inputType = "Manual";
+            }
+        });
+    }
+
+    public open(): Promise<IBarcodeMsrDialogResult> {
+        let promise: Promise<IBarcodeMsrDialogResult> = new Promise((resolve: BarcodeMsrDialogResolveFunction, reject: BarcodeMsrDialogRejectFunction) => {
+            this.resolve = resolve;
+            let option: ITemplatedDialogOptions = {
+                title: "Barcode Scanner and MSR Swipe Dialog",
+                onCloseX: this._cancelButtonClickHandler.bind(this)
+            };
+
+            this.openDialog(option);
+        });
+
+        return promise;
+    }
+
+    private _cancelButtonClickHandler(): boolean {
+        this._resolvePromise({ canceled: true });
+        return false;
+    }
+
+    private _resolvePromise(result: IBarcodeMsrDialogResult): void {
+        if (ObjectExtensions.isFunction(this.resolve)) {
+            this.resolve(result);
+
+            this.resolve = null;
+            this.closeDialog();
+        }
     }
 
 }
---
+++ BarcodeMsrDialogSample/Pos/FulfillmentLineCommand.ts
@@ -0,0 +1,66 @@
+﻿
+import { ClientEntities, ProxyEntities } from "PosApi/Entities";
+import { IExtensionCommandContext } from "PosApi/Extend/Views/AppBarCommands";
+import { ArrayExtensions, ObjectExtensions, StringExtensions } from "PosApi/TypeExtensions";
+
+import {
+    FulfillmentLineExtensionCommandBase,
+    FulfillmentLinesSelectedData,
+    FulfillmentLinePackingSlipSelectedData,
+    FulfillmentLinesLoadedData,
+    IFulfillmentLineExtensionCommandState,
+    IExtensionCommandToFulfillmentLineMessageTypeMap,
+    IFulfillmentLineToExtensionCommandMessageTypeMap
+} from "PosApi/Extend/Views/FulfillmentLineView";
+
+import { ShowMessageDialogClientRequest, ShowMessageDialogClientResponse, IMessageDialogOptions } from "PosApi/Consume/Dialogs";
+
+import { GetScanResultClientRequest, GetScanResultClientResponse } from "PosApi/Consume/ScanResults";
+
+import BarcodeMsrDialog from "./BarcodeMsrDialog";
+
+export default class FulfillmentLineCommand extends FulfillmentLineExtensionCommandBase {
+
+    private _fulfillmentLines: ProxyEntities.FulfillmentLine[];
+    private _selectedFulfillmentLines: ProxyEntities.FulfillmentLine[];
+
+    constructor(context: IExtensionCommandContext<IFulfillmentLineToExtensionCommandMessageTypeMap>) {
+        super(context);
+
+        this.id = "sampleFufillmentLineCommand";
+        this.label = "Scan and Select Product";
+        this.extraClass = "iconLightningBolt";
+
+        this._selectedFulfillmentLines = [];
+        this.fulfillmentLinesSelectionHandler = (data: FulfillmentLinesSelectedData): void => {
+            this._selectedFulfillmentLines = data.fulfillmentLines;
+        }
+
+        this.fulfillmentLinesSelectionClearedHandler = (): void => {
+            this._selectedFulfillmentLines = [];
+        }
+
+        this.packingSlipSelectedHandler = (data: FulfillmentLinePackingSlipSelectedData): void => {
+            this.isVisible = false;
+        }
+
+        this.packingSlipSelectionClearedHandler = (): void => {
+            this.isVisible = false;
+        }
+
+        this.fulfillmentLinesLoadedHandler = (data: FulfillmentLinesLoadedData): void => {
+            this._fulfillmentLines = data.fulfillmentLines;
+        }
+    }
+
+    protected init(state: IFulfillmentLineExtensionCommandState): void {
+        this.isVisible = true;
+        this.canExecute = true;
+    }
+
+
+    protected execute(): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
