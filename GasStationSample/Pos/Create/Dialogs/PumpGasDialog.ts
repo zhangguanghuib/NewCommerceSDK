8490ba7835d3a64ae0ffdb73e0cdbb584d142c4b
zhangguanghuib <=>
2022-09-20 18:03:38 +0800

    Update PumpGasDialog.ts

--- GasStationSample/Pos/Create/Dialogs/PumpGasDialog.ts
+++ GasStationSample/Pos/Create/Dialogs/PumpGasDialog.ts
@@ -1,9 +1,12 @@
 ﻿import * as Dialogs from "PosApi/Create/Dialogs";
 import { Entities } from "../../DataService/DataServiceEntities.g";
+import { ClientEntities } from "PosApi/Entities";
 import ko from "knockout";
 import { CurrencyFormatter } from "PosApi/Consume/Formatters";
 import { NumberFormattingHelper } from "../../NumberFormattingHelper";
 import { GasStationDataStore } from "../../GasStationDataStore";
+import { ArrayExtensions, ObjectExtensions } from "PosApi/TypeExtensions";
+import { GasPumpStatus } from "../../GasStationTypes";
 
 export default class PumpGasDialog extends Dialogs.ExtensionTemplatedDialogBase {
     public gasPump: Entities.GasPump;
@@ -33,9 +36,72 @@ export default class PumpGasDialog extends Dialogs.ExtensionTemplatedDialogBase 
         this.isEmergencyInProgress = ko.Observable(false);
     }
 
+    public open(options: { pumpId: number }): Promise<ClientEntities.ICancelableDataResult<number>> {
+        this.gasPump = ArrayExtensions.firstOrUndefined(GasStationDataStore.instance.pumps, (pump): boolean => { return pump.Id === options.pumpId });
+        if (ObjectExtensions.isNullOrUndefined(this.gasPump)) {
+            return Promise.reject(new Error("Pump not found"));
+        }
+
+        let intervalId = setInterval(() => {
+            let updatedQuantity: number = this._pumpedQuantity() + 0.033;
+            this._pumpedQuantity(updatedQuantity);
+
+            if (updatedQuantity > 75) {
+                this.isEmergencyInProgress(true);
+                GasStationDataStore.instance.updatePumpStatusAsync(this.context, options.pumpId, { GasPumpStatusValue: GasPumpStatus.Emergency, LastUpdateTime: new Date(), SaleTotal: 0, SaleVolume: 0 });
+                clearInterval(intervalId);
+                intervalId = undefined;
+            }
+        }, 50);
+
+        let promise: Promise<void> = new Promise<void>((resolve: () => void, reject: (reason: any) => void): void => {
+            this._resolve = resolve;
+            let option: Dialogs.ITemplatedDialogOptions = {
+                title: this.gasPump.Name,
+                onCloseX: this.onCloseX.bind(this),
+                button1: {
+                    id: "OkayButton",
+                    label: "Stop pumping",
+                    isPrimary: true,
+                    onClick: this._stopPumpClickHandler.bind(this)
+                }
+            };
+
+            this.openDialog(option);
+        });
+
+        return promise.then((): ClientEntities.ICancelableDataResult<number> => {
+            if (typeof intervalId === "number") {
+                clearInterval(intervalId);
+            }
+
+            let finalQuantity: number = this._pumpedQuantity();
+            return { canceled: this._canceled, data: finalQuantity };
+        });
+    }
+
 
     public onReady(element: HTMLElement): void {
         ko.applyBindings(this, element);
+    }
+
+    private onCloseX(): boolean {
+        this._canceled = true;
+        this._resolvePromise();
+        return true;
+    }
+
+    private _stopPumpClickHandler(): boolean {
+        this._resolvePromise();
+        return true;
+    }
+
+    private _resolvePromise(): void {
+        if (ObjectExtensions.isFunction(this._resolve)) {
+            this._resolve();
+
+            this._resolve = null;
+        }
     }
 
 }
