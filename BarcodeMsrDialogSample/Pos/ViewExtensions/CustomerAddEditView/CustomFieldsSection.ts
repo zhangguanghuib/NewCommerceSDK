aaac80011f3f8f5426f9799b9af91904b3083159
zhangguanghuib <=>
2022-09-27 14:38:18 +0800

    Add knockout supported

---
+++ BarcodeMsrDialogSample/Pos/ViewExtensions/CustomerAddEditView/CustomFieldsSection.ts
@@ -0,0 +1,20 @@
+﻿import ko from "knockout";
+
+import {
+    CustomerAddEditCustomControlBase
+} from "PosApi/Extend/Views/CustomerAddEditView";
+
+export default class CustomFieldsSection extends CustomerAddEditCustomControlBase {
+
+    public ssn: ko.Observable<string>;
+
+    protected init(state: Commerce.Extensibility.ICustomerAddEditCustomControlState): void {
+
+    }
+
+
+    onReady(element: HTMLElement): void {
+
+    }
+
+}
