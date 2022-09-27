52a8e54c816ff4ccdd4570492683f81e63110bd2
zhangguanghuib <=>
2022-09-27 13:19:29 +0800

    Hardware station .net Framework supported

---
+++ BarcodeMsrDialogSample/Pos/ViewExtensions/CustomerAddEditView/MCOBirthdayFieldsSection.ts
@@ -0,0 +1,13 @@
+﻿import { CustomerAddEditCustomControlBase } from "PosApi/Extend/Views/CustomerAddEditView";
+
+export default class MCOBirthdayFieldsSecion extends CustomerAddEditCustomControlBase {
+
+    protected init(state: Commerce.Extensibility.ICustomerAddEditCustomControlState): void {
+        throw new Error("Method not implemented.");
+    }
+
+    onReady(element: HTMLElement): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
