2ec4b3b6eecb7d28d0132945d3035b2d98328d4e
zhangguanghuib <=>
2022-10-24 17:56:11 +0800

    add Extend Folder

---
+++ GasStationSample/Pos/Extend/Triggers/InitializeStationTrigger.ts
@@ -0,0 +1,19 @@
+﻿import { ApplicationStartTrigger } from "PosApi/Extend/Triggers/ApplicationTriggers";
+import { GasStationDataStore } from "../../GasStationDataStore";
+import ko from "knockout"
+
+export default class InitializeStationTrigger extends ApplicationStartTrigger {
+
+    public execute(options: Commerce.Triggers.IApplicationStartTriggerOptions): Promise<void> {
+        ko.bindingHandlers.__postStopExtensionBinding = {
+            init: () => {
+                return {
+                    controlsDescendantBindings: true
+                }
+            }
+        };
+
+        return GasStationDataStore.instance.initAsync(this.context);
+    }
+
+}
---
+++ GasStationSample/Pos/Extend/Views/PumpGasExtensionCommand.ts
@@ -0,0 +1,13 @@
+﻿import { SimpleProductDetailsExtensionCommandBase } from "PosApi/Extend/Views/SimpleProductDetailsView";
+
+export default class PumpGasExtensionCommand extends SimpleProductDetailsExtensionCommandBase {
+
+    protected init(state: Commerce.Extensibility.ISimpleProductDetailsExtensionCommandState): void {
+        throw new Error("Method not implemented.");
+    }
+
+    protected execute(): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
