43a4202922296e986789a1182452ddefa591ae5f
zhangguanghuib <=>
2022-11-04 06:56:00 +0800

    POS Sample Extension added

---
+++ BarcodeMsrDialogSample/PosSamples/Views/Samples/DynamicDataListView.ts
@@ -0,0 +1,14 @@
+﻿import * as Views from "PosApi/Create/Views";
+
+export default class DynamicDataListView extends Views.CustomViewControllerBase {
+
+    public dispose(): void {
+        throw new Error("Method not implemented.");
+    }
+
+
+    public onReady(element: HTMLElement): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
