ceb378d968af6a23820938b8710d4830c92a92fa
zhangguanghuib <=>
2022-11-05 12:50:29 +0800

    Samples View Commandbutton

---
+++ BarcodeMsrDialogSample/PosSamples/ViewExtensions/Search/NavigateToSimpleExtensionViewCommand.ts
@@ -0,0 +1,18 @@
+﻿import { IExtensionCommandContext } from "PosApi/Extend/Views/AppBarCommands";
+import * as SearchView from "PosApi/Extend/Views/SearchView";
+
+export default class NavigateToSimpleExtensionViewCommand extends SearchView.ProductSearchExtensionCommandBase {
+
+    constructor(context: IExtensionCommandContext<SearchView.IProductSearchToExtensionCommandMessageTypeMap>) {
+        super(context);
+    }
+
+    protected init(state: SearchView.IProductSearchExtensionCommandState): void {
+        throw new Error("Method not implemented.");
+    }
+
+    protected execute(): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
--- BarcodeMsrDialogSample/PosSamples/Views/SamplesView.ts
+++ BarcodeMsrDialogSample/PosSamples/Views/SamplesView.ts
@@ -1,4 +1,7 @@
-﻿import * as Views from "PosApi/Create/Views";
+﻿import ko from "knockout";
+
+import * as Views from "PosApi/Create/Views";
+import { ObjectExtensions } from "PosApi/TypeExtensions";
 
 export interface ISampleItem {
     label: string,
@@ -8,12 +11,33 @@ export interface ISampleItem {
 
 export default class SamplesView extends Views.CustomViewControllerBase {
 
+    public readonly samplesList: ISampleItem[];
+
+    constructor(context: Views.ICustomViewControllerContext) {
+        super(context);
+
+        this.state.title = "Samples";
+
+        this.samplesList = [
+            {
+                label: "Pos Controls",
+                items: [
+                    { label: "DataList (dynamic)", viewName: "DynamicDataListView" }
+                ]
+            }
+        ];
+    }
+
     public dispose(): void {
-        throw new Error("Method not implemented.");
+        ObjectExtensions.disposeAllProperties(this);
     }
 
     public onReady(element: HTMLElement): void {
-        throw new Error("Method not implemented.");
+        ko.applyBindings(this, element);
+    }
+
+    public sampleClick(sampleItem: ISampleItem) {
+        this.context.navigator.navigate(sampleItem.viewName);
     }
 
 }
