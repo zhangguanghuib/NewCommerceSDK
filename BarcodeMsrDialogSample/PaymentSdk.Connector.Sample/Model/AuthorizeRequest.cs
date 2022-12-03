0d607034ae3958558cf2d636ae92db5bc1f7bab2
Guanghui Zhang <guazha@microsoft.com>
2022-12-02 22:37:43 -0800

    Initialize Payment connector project structure

---
+++ BarcodeMsrDialogSample/PaymentSdk.Connector.Sample/Model/AuthorizeRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentSdk.Connector.Sample.Model
+{
+    class AuthorizeRequest
+    {
+    }
+}
---
+++ BarcodeMsrDialogSample/PaymentSdk.Connector.Sample/Utilities/PaymentUtilities.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentSdk.Connector.Sample.Utilities
+{
+    class PaymentUtilities
+    {
+    }
+}
