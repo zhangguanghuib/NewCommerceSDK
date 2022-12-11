b54b8e02479039294af1378ff380f680adb365fd
zhangguanghuib <=>
2022-12-11 14:12:58 +0800

    Gas Pump Status

--- GasStationSample/Pos/Extend/Header/GasPumpStatusPackingItem.ts
+++ GasStationSample/Pos/Extend/Header/GasPumpStatusPackingItem.ts
@@ -6,7 +6,7 @@ import ko from "knockout";
 import { GasPumpStatus } from "GasStationTypes";
 
 import GasPump = Entities.GasPump;
-import { PumpStatusChangedHandler } from "GasStationDataStore";
+import { GasStationDataStore, PumpStatusChangedHandler } from "GasStationDataStore";
 
 export default class GasPumpStatusPackingItem extends CustomPackingItem {
 
@@ -57,11 +57,64 @@ export default class GasPumpStatusPackingItem extends CustomPackingItem {
     }
 
     public onReady(packedElement: HTMLElement, unpackedElement: HTMLElement): void {
-        
+        ko.applyBindingsToNode(unpackedElement, {
+            template: {
+                name: "Microsoft_Pos_GasStationHeaderExtensionSample_UnpackedGasPumpStatusItem",
+                data: this
+            }
+        });
+
+        ko.applyBindingsToNode(packedElement, {
+            template: {
+                name: "Microsoft_PosGasStationHeaderExtensionSample_PackedGasPumpStatusItem",
+                data: this
+            }
+        });
     }
 
     protected init(state: Commerce.Extensibility.ICustomHeaderPackingItemState): void {
+        this.visible = true;
+        this._pumps = GasStationDataStore.instance.pumps;
+        this._reevaluatePumpStatuses();
+        this._pumpStatusChangedHandlerProxied = (pumps: GasPump[]): void => {
+            this._pumps = pumps;
+            this._reevaluatePumpStatuses();
+        }
+
+        GasStationDataStore.instance.addPumpStatusChangedHandler(this._pumpStatusChangedHandlerProxied);
         
+    }
+
+    public dispose(): void {
+        super.dispose();
+    }
+
+    public onItemClickedHandler(): void {
+        this.context.navigator.navigate("GasPumpStatusView");
+    }
+
+    private _reevaluatePumpStatuses(): void {
+        const newStatus: GasPumpStatus = this._getGeneralPumpsStatus(this._pumps);
+        this._overallStatus(newStatus);
+    }
+
+    private _getGeneralPumpsStatus(pumps: GasPump[]): GasPumpStatus {
+        let newStatus: GasPumpStatus = GasPumpStatus.Unknown;
+
+        for (let i = 0; i < pumps.length; ++i) {
+            const pumpState: GasPumpStatus = pumps[i].State.GasPumpStatusValue;
+            switch (pumpState) {
+                case GasPumpStatus.Emergency:
+                    return GasPumpStatus.Emergency;
+                case GasPumpStatus.Stopped:
+                    newStatus = GasPumpStatus.Stopped;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return newStatus;
     }
 
 }
--- GasStationSample/Pos/Extend/RequestHandlers/KeyInGasolineQuantityRequestHandler.ts
+++ GasStationSample/Pos/Extend/RequestHandlers/KeyInGasolineQuantityRequestHandler.ts
@@ -1,9 +1,42 @@
-﻿import { GetKeyedInQuantityClientRequestHandler } from "PosApi/Extend/RequestHandlers/CartRequestHandlers";
+﻿import { GetCurrentCartClientRequest, GetCurrentCartClientResponse, GetKeyedInQuantityClientRequest, GetKeyedInQuantityClientResponse } from "PosApi/Consume/Cart";
+import { ClientEntities, ProxyEntities } from "PosApi/Entities";
+import { GetKeyedInQuantityClientRequestHandler } from "PosApi/Extend/RequestHandlers/CartRequestHandlers";
+import { ArrayExtensions, ObjectExtensions, StringExtensions } from "PosApi/TypeExtensions";
+
+import { GasStationDataStore } from "../../GasStationDataStore";
+import { GASOLINE_QUANTITY_EXTENSION_PROPERTY_NAME } from "../../GlobalConstants";
+import { NumberFormattingHelper } from "../../NumberFormattingHelper";
+
+type RequestHandlerResult = ClientEntities.ICancelableDataResult<GetKeyedInQuantityClientResponse>;
 
 export default class AutomatedKeyInGasolineQuantityRequestHandler extends GetKeyedInQuantityClientRequestHandler {
 
-    public executeAsync(request: Commerce.GetKeyedInQuantityClientRequest<Commerce.GetKeyedInQuantityClientResponse>): Promise<Commerce.Client.Entities.ICancelableDataResult<Commerce.GetKeyedInQuantityClientResponse>> {
-        throw new Error("Method not implemented.");
+    public executeAsync(request: GetKeyedInQuantityClientRequest<GetKeyedInQuantityClientResponse>): Promise<RequestHandlerResult> {
+
+        if (StringExtensions.compare(request.product.ItemId, GasStationDataStore.instance.gasStationDetails.GasolineItemId, true) !== 0) {
+            return this.defaultExecuteAsync(request);
+        }
+
+        let getCartRequest: GetCurrentCartClientRequest<GetCurrentCartClientResponse> = new GetCurrentCartClientRequest(request.correlationId);
+        return this.context.runtime.executeAsync(getCartRequest)
+            .then((result: ClientEntities.ICancelableDataResult<GetCurrentCartClientResponse>): Promise<RequestHandlerResult> => {
+                if (result.canceled) {
+                    return Promise.resolve({ canceled: true, data: null });
+                }
+
+                let cart: ProxyEntities.Cart = result.data.result;
+                let quantityExtensionProperty: ProxyEntities.CommerceProperty
+                    = ArrayExtensions.firstOrUndefined(cart?.ExtensionProperties, (p) => p.Key === GASOLINE_QUANTITY_EXTENSION_PROPERTY_NAME);
+
+                if (!StringExtensions.isNullOrWhitespace(cart.Id) && !ObjectExtensions.isNullOrUndefined(quantityExtensionProperty)) {
+                    let decimalValue: number = NumberFormattingHelper.roundToNdigits(parseFloat(quantityExtensionProperty.Value.StringValue), 3);
+                    return Promise.resolve({ canceled: false, data: new GetKeyedInQuantityClientResponse(decimalValue) });
+                } else {
+                    return this.defaultExecuteAsync(request);
+                }
+
+            });
+        
     }
 
 }
