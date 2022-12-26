9e721a80799af1d4f09ed15d271100899381a1c3
zhangguanghuib <=>
2022-12-26 13:53:39 +0800

    Bank Drop Number Dialog Skip

---
+++ BarcodeMsrDialogSample/Pos/RequestHandlers/GetBankBagNumberClientRequestHandlerExt.ts
@@ -0,0 +1,21 @@
+﻿import { GetBankBagNumberClientRequestHandler } from "PosApi/Extend/RequestHandlers/StoreOperationsRequestHandlers";
+import { GetBankBagNumberClientRequest, GetBankBagNumberClientResponse } from "PosApi/Consume/StoreOperations";
+import { ClientEntities } from "PosApi/Entities";
+
+export default class GetBankBagNumberClientRequestHandlerExt extends GetBankBagNumberClientRequestHandler {
+
+    public executeAsync(request: GetBankBagNumberClientRequest<GetBankBagNumberClientResponse>):
+        Promise<ClientEntities.ICancelableDataResult<GetBankBagNumberClientResponse>> {
+
+        const isoStr = new Date().toISOString();
+
+        let response: GetBankBagNumberClientResponse = new GetBankBagNumberClientResponse({ bagNumber: isoStr });
+
+        return Promise.resolve(<ClientEntities.ICancelableDataResult<GetBankBagNumberClientResponse>>{
+            canceled: false,
+            data: response
+        });
+
+        // return this.defaultExecuteAsync(request);
+    }
+}
