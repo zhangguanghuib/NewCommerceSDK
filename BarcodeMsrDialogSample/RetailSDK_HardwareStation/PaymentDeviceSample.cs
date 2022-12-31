bb7a9835bc2571d905692fb12396d40f5d7d686d
zhangguanghuib <=>
2022-12-31 19:26:48 +0800

    HWS Samples

---
+++ BarcodeMsrDialogSample/RetailSDK_HardwareStation/PaymentDeviceSample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetailSDK.HardwareStation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Dynamics.Commerce.HardwareStation;
+    using Microsoft.Dynamics.Commerce.HardwareStation.CardPayment;
+    using Microsoft.Dynamics.Commerce.HardwareStation.PeripheralRequests;
+    using Microsoft.Dynamics.Commerce.HardwareStation.Peripherals;
+    using Microsoft.Dynamics.Commerce.HardwareStation.Peripherals.Entities;
+    using Microsoft.Dynamics.Commerce.HardwareStation.Peripherals.PaymentTerminal;
+    using Microsoft.Dynamics.Commerce.Runtime.Handlers;
+    using Microsoft.Dynamics.Commerce.Runtime.Messages;
+    //using Microsoft.Dynamics.Commerce.VirtualPeripherals.Framework;
+    //using Microsoft.Dynamics.Commerce.VirtualPeripherals.MessagePipelineHandler;
+    //using Microsoft.Dynamics.Commerce.VirtualPeripherals.MessagePipelineProxy;
+    using Microsoft.Dynamics.Retail.PaymentSDK.Portable.Constants;
+    using Newtonsoft.Json.Linq;
+    using PSDK = Microsoft.Dynamics.Retail.PaymentSDK.Portable;
+    public class PaymentDeviceSample
+    {
+
+    }
+}
