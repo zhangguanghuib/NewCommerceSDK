3d19058be19f31acb1a331ca75aa319a0229d7fe
zhangguanghuib <=>
2023-01-13 16:34:30 +0800

    Telstra Info Code Validation

---
+++ BarcodeMsrDialogSample/CommerceRuntime/Triggers/GetReasonCodesServiceRequestTrigger.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Dynamics.Commerce.Runtime;
+using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+using Microsoft.Dynamics.Commerce.Runtime.DataServices.Messages;
+using Microsoft.Dynamics.Commerce.Runtime.Messages;
+using Microsoft.Dynamics.Commerce.Runtime.Services.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace GHZ.BarcodeMsrDialogSample.CommerceRuntime.Triggers
+{
+    public class GetReasonCodesServiceRequestTrigger : IRequestTriggerAsync
+    {
+        public IEnumerable<Type> SupportedRequestTypes
+        {
+            get
+            {
+                return new[] { typeof(GetReasonCodesServiceRequest) };
+            }
+        }
+
+        public async Task OnExecuted(Request request, Response response)
+        {
+            GetReasonCodesServiceResponse getReasonCodesServiceResponse = response as GetReasonCodesServiceResponse;
+            PagedResult<ReasonCode> reasonCodes = getReasonCodesServiceResponse.ReasonCodes;
+            foreach (ReasonCode reasonCode in reasonCodes)
+            {
+                if (reasonCode.ReasonCodeId.Equals("MatchPrice"))
+                {
+                    reasonCode.ExtensionProperties.Add(new CommerceProperty("BeginWith", "LOY"));
+                }
+            }
+
+            await Task.FromResult(getReasonCodesServiceResponse).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Pre trigger code
+        /// </summary>
+        /// <param name="request">The request.</param>
+        public async Task OnExecuting(Request request)
+        {
+            await Task.CompletedTask.ConfigureAwait(false);
+        }
+    }
+}
---
+++ BarcodeMsrDialogSample/Pos/TriggerHandlers/PostGetReasonCodeLineTrigger.ts
@@ -0,0 +1,21 @@
+﻿import * as Triggers from "PosApi/Extend/Triggers/ReasonCodeTriggers";
+import { ClientEntities, ProxyEntities } from "PosApi/Entities";
+import { ObjectExtensions, StringExtensions} from "PosApi/TypeExtensions";
+
+export default class PostGetReasonCodeLineTrigger extends Triggers.PostGetReasonCodeLineTrigger {
+    public execute(options: Triggers.IPostGetReasonCodeLineTriggerOptions): Promise<ClientEntities.ICancelable> {
+
+        let prop  =  options.reasonCode.ExtensionProperties.filter((prop: ProxyEntities.CommerceProperty) => prop.Key === "BeginWith")
+            .map((prop: ProxyEntities.CommerceProperty) => prop.Value)[0];
+
+        if (!ObjectExtensions.isNullOrUndefined(prop)) {
+            if (prop.StringValue !== "" && StringExtensions.beginsWith(options.reasonCodeLine.Information, prop.StringValue, false)) {
+                return Promise.resolve({ canceled: false, data: options });
+            } else {
+                return Promise.reject(new ClientEntities.ExtensionError("The input information does not match require pattern"));
+            }
+        } else {
+            return Promise.resolve({ canceled: false, data: options });
+        }
+    }
+}
