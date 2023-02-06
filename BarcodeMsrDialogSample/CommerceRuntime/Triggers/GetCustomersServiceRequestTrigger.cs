2006fc918c7592d8a189b9d0019541bdd79e95a6
zhangguanghuib <=>
2023-02-06 21:47:23 +0800

    Fill Receipt Email

---
+++ BarcodeMsrDialogSample/CommerceRuntime/Triggers/GetCustomersServiceRequestTrigger.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Dynamics.Commerce.Runtime;
+using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+using Microsoft.Dynamics.Commerce.Runtime.Messages;
+using Microsoft.Dynamics.Commerce.Runtime.Services.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GHZ.BarcodeMsrDialogSample.CommerceRuntime.Triggers
+{
+    public class GetCustomersServiceRequestTrigger : IRequestTriggerAsync
+    {
+        public IEnumerable<Type> SupportedRequestTypes
+        {
+            get
+            {
+                return new[] { typeof(GetCustomersServiceRequest) };
+            }
+        }
+
+        public async Task OnExecuted(Request request, Response response)
+        {
+            GetCustomersServiceResponse getCustomersServiceResponse = response as GetCustomersServiceResponse;
+            PagedResult<Customer> customers = getCustomersServiceResponse.Customers;
+            Customer customer= customers.FirstOrDefault();
+
+            if(customer != null && String.IsNullOrWhiteSpace(customer.ReceiptEmail) && String.IsNullOrWhiteSpace(customer.Email))
+            {
+                customer.ReceiptEmail = "guanghui01@microsoft.com";
+            }
+
+            await Task.FromResult(getCustomersServiceResponse).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Pre trigger code
+        /// </summary>
+        /// <param name="request">The request.</param>
+        public async Task OnExecuting(Request request)
+        {
+            await Task.CompletedTask.ConfigureAwait(false);
+        }
+    }
+}
---
+++ BarcodeMsrDialogSample/Pos/RequestHandlers/GetReceiptEmailAddressClientRequestHandlerExt.ts
@@ -0,0 +1,44 @@
+﻿import { GetReceiptEmailAddressClientRequestHandler } from "PosApi/Extend/RequestHandlers/CartRequestHandlers";
+import { ClientEntities, ProxyEntities} from "PosApi/Entities";
+import { StringExtensions } from "PosApi/TypeExtensions";
+import {
+    GetCurrentCartClientRequest, GetCurrentCartClientResponse
+} from "PosApi/Consume/Cart";
+import {
+    GetCustomerClientRequest, GetCustomerClientResponse,
+} from "PosApi/Consume/Customer";
+import { ObjectExtensions } from "PosApi/TypeExtensions";
+
+export default class GetReceiptEmailAddressClientRequestHandlerExt extends GetReceiptEmailAddressClientRequestHandler {
+    public executeAsync(request: Commerce.GetReceiptEmailAddressClientRequest<Commerce.GetReceiptEmailAddressClientResponse>):
+        Promise<ClientEntities.ICancelableDataResult<Commerce.GetReceiptEmailAddressClientResponse>> {
+
+        let currentCart: ProxyEntities.Cart;
+        return this.context.runtime.executeAsync<GetCurrentCartClientResponse>(new GetCurrentCartClientRequest())
+            .then((getCurrentCartClientResponse: ClientEntities.ICancelableDataResult<GetCurrentCartClientResponse>):
+                Promise<ClientEntities.ICancelableDataResult<GetCustomerClientResponse>> => {
+                currentCart = getCurrentCartClientResponse.data.result;
+                // Gets the current customer.
+                let result: Promise<ClientEntities.ICancelableDataResult<GetCustomerClientResponse>>;
+                if (!ObjectExtensions.isNullOrUndefined(currentCart) && !ObjectExtensions.isNullOrUndefined(currentCart.CustomerId)) {
+                    let getCurrentCustomerClientRequest: GetCustomerClientRequest<GetCustomerClientResponse> =
+                        new GetCustomerClientRequest(currentCart.CustomerId);
+                    result = this.context.runtime.executeAsync<GetCustomerClientResponse>(getCurrentCustomerClientRequest);
+                } else {
+                    result = Promise.resolve({ canceled: false, data: new GetCustomerClientResponse(null) });
+                }
+                return result;
+            })
+            .then((getCurrentCustomerClientResponse: ClientEntities.ICancelableDataResult<GetCustomerClientResponse>):
+                Promise<ProxyEntities.Customer> => {
+                let currentCustomer: ProxyEntities.Customer = getCurrentCustomerClientResponse.data.result;
+                return Promise.resolve(currentCustomer);
+            }).then((currentCustomer: ProxyEntities.Customer) => {
+                if (StringExtensions.isEmptyOrWhitespace(currentCustomer.Email) || StringExtensions.isEmptyOrWhitespace(currentCustomer.ReceiptEmail)) {
+                    currentCustomer.ReceiptEmail = "guazha03@microsoft.com";
+                }
+                return this.defaultExecuteAsync(request);
+            });
+    }
+
+}
