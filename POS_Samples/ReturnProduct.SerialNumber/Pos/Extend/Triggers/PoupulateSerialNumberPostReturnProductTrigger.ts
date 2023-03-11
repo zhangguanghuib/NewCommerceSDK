4ac5ddb862923c64e853f6d7cd9780a669b5fb43
zhangguanghuib <=>
2023-03-11 16:33:41 +0800

    Product Return

---
+++ POS_Samples/ReturnProduct.SerialNumber/Pos/Extend/Triggers/PoupulateSerialNumberPostReturnProductTrigger.ts
@@ -0,0 +1,27 @@
+﻿import * as Products from "PosApi/Consume/Products";
+import { PostReturnProductTrigger } from "PosApi/Extend/Triggers/ProductTriggers";
+import { ClientEntities} from "PosApi/Entities";
+
+export default class PoupulateSerialNumberPostReturnProductTrigger extends PostReturnProductTrigger {
+    public execute(options: Commerce.Triggers.IPostReturnProductTriggerOptions): Promise<void> {
+        console.log("PostReturnProductTrigger");
+
+        if (options.cartLinesForReturn.length <= 0) {
+            return Promise.resolve();
+        }
+
+        let productIds: number[] = [options.cartLinesForReturn[0].ProductId];
+        let getProductsByIdsClientRequest: Products.GetProductsByIdsClientRequest<Products.GetProductsByIdsClientResponse> =
+            new Products.GetProductsByIdsClientRequest(productIds);
+
+        this.context.runtime.executeAsync(getProductsByIdsClientRequest)
+            .then((getProductsByIdsClientResponse: ClientEntities.ICancelableDataResult<Products.GetProductsByIdsClientResponse>) => {
+
+            })
+
+        let getSerialNumberClientRequest: Products.GetSerialNumberClientRequest = null;
+
+        return Promise.resolve();
+    }
+
+}
