ac5c36193e3302b2f86c09647d7fc621d863c313
zhangguanghuib <=>
2023-03-17 23:14:24 +0800

    Cart Deletion Sample

---
+++ POS_Samples/CartDeletion/CommerceRuntime/NonBindableOperationController.Carts.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contoso.GasStationSample.CommerceRuntime
+{
+    using Microsoft.Dynamics.Commerce.Runtime;
+    using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+    using Microsoft.Dynamics.Commerce.Runtime.DataServices.Messages;
+    using Microsoft.Dynamics.Commerce.Runtime.Hosting.Contracts;
+    using Microsoft.Dynamics.Commerce.Runtime.Messages;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class NonBindableOperationController:  IController
+    {
+        [HttpPost]
+        [Authorization(CommerceRoles.Device, CommerceRoles.Employee, CommerceRoles.Anonymous, CommerceRoles.Application)]
+        public async Task<PagedResult<Cart>> SearchCartsAsync(IEndpointContext context, 
+            DateTimeOffset lastModifiedDateTimeFrom,                                                          
+            DateTimeOffset lastModifiedDateTimeTo,
+            QueryResultSettings queryResultSettings)
+        {
+            var cartSearchCriteria = new CartSearchCriteria
+            {
+                CartTypeValue = (int)CartType.Shopping,
+                LastModifiedDateTimeFrom = lastModifiedDateTimeFrom,
+                LastModifiedDateTimeTo = lastModifiedDateTimeTo,
+                IncludeAnonymous = true,
+                SuspendedOnly = false
+            };
+
+            queryResultSettings.Paging = PagingInfo.AllRecords;
+
+            var sortColumns = new ObservableCollection<SortColumn>();
+            sortColumns.Add(new SortColumn(columnName: "ModifiedDateTime", isDescending: false ));
+            queryResultSettings.Sorting = new SortingInfo()
+            {
+                Columns = sortColumns
+            };
+
+            var getCartRequest = new GetCartRequest(cartSearchCriteria, queryResultSettings);
+            var response = await context.ExecuteAsync<GetCartResponse>(getCartRequest).ConfigureAwait(false);
+
+            return new PagedResult<Cart>(response.Carts.AsReadOnly());
+        }
+
+        [HttpPost]
+        [Authorization(CommerceRoles.Device, CommerceRoles.Employee, CommerceRoles.Anonymous, CommerceRoles.Application)]
+        public async Task<PagedResult<Cart>> DeleteCartsAsync(IEndpointContext context,
+            DateTimeOffset lastModifiedDateTimeFrom,
+            DateTimeOffset lastModifiedDateTimeTo, 
+            QueryResultSettings queryResultSettings)
+        {
+            var cartSearchCriteria = new CartSearchCriteria
+            {
+                CartTypeValue = (int)CartType.Shopping,
+                LastModifiedDateTimeFrom = lastModifiedDateTimeFrom,
+                LastModifiedDateTimeTo = lastModifiedDateTimeTo,
+                IncludeAnonymous = true,
+                SuspendedOnly = false
+            };
+
+            queryResultSettings.Paging = PagingInfo.AllRecords;
+
+            var sortColumns = new ObservableCollection<SortColumn>();
+            sortColumns.Add(new SortColumn(columnName: "ModifiedDateTime", isDescending: false));
+            queryResultSettings.Sorting = new SortingInfo()
+            {
+                Columns = sortColumns
+            };
+
+            var getCartRequest = new GetCartRequest(cartSearchCriteria, queryResultSettings);
+            var response = await context.ExecuteAsync<GetCartResponse>(getCartRequest).ConfigureAwait(false);
+
+            IEnumerable<string> cartIds = response.Carts.Select(cart => cart.Id);
+
+            DeleteCartDataRequest request = new DeleteCartDataRequest(cartIds.Where(id => !string.IsNullOrWhiteSpace(id)));
+            await context.ExecuteAsync<NullResponse>(request).ConfigureAwait(false);
+
+            return new PagedResult<Cart>(response.Carts.AsReadOnly());
+        }
+    }
+}
---
+++ POS_Samples/CartDeletion/Pos/Extend/ViewExtensions/ManageShifts/ManageShiftsUpdCommand.ts
@@ -0,0 +1,38 @@
+﻿import * as ManageShiftsView from "PosApi/Extend/Views/ManageShiftsView";
+import { IExtensionCommandContext } from "PosApi/Extend/Views/AppBarCommands";
+import { Icons } from "PosApi/Create/Views";
+import { ClientEntities } from "PosApi/Entities";
+
+import { StoreOperations } from "../../../DataService/DataServiceRequests.g";
+
+export default class ManageShiftsUpdCommand extends ManageShiftsView.ManageShiftsExtensionCommandBase {
+
+    constructor(context: IExtensionCommandContext<ManageShiftsView.IManageShiftsToExtensionCommandMessageTypeMap>) {
+        super(context);
+        this.label = "Delete Cart";
+        this.id = "DelCart";
+        this.extraClass = Icons.Delete;
+    }
+
+    protected init(state: ManageShiftsView.IManageShiftsExtensionCommandState): void {
+        this.isVisible = true;
+        this.canExecute = true;
+    }
+
+    protected execute(): void {
+        //console.log("delete cart");
+        let toDate: Date = new Date()
+        let fromDate: Date = new Date(toDate.getDate() - 3);
+        let searchCartsAsyncRequest: StoreOperations.SearchCartsAsyncRequest<StoreOperations.SearchCartsAsyncResponse>
+            = new StoreOperations.SearchCartsAsyncRequest<StoreOperations.SearchCartsAsyncResponse>(fromDate, toDate);
+
+        this.context.runtime.executeAsync(searchCartsAsyncRequest).then(
+            (response: ClientEntities.ICancelableDataResult<StoreOperations.SearchCartsAsyncResponse>): Promise<string> => {
+                let cartIds = response.data.result.map(c => c.Id).join('');
+                return Promise.resolve(cartIds);
+            }).then((cartId: string): void => {
+                console.log(cartId);
+            });
+    }
+
+}
