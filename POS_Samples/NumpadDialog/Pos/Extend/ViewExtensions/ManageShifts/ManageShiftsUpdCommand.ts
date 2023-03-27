185d524e787c36c5de5ad2c3aabfe9625225b7e6
zhangguanghuib <=>
2023-03-27 10:23:25 +0800

    NumberDialog

---
+++ POS_Samples/NumpadDialog/Pos/Extend/ViewExtensions/ManageShifts/ManageShiftsUpdCommand.ts
@@ -0,0 +1,23 @@
+﻿import * as ManageShiftsView from "PosApi/Extend/Views/ManageShiftsView";
+import { IExtensionCommandContext } from "PosApi/Extend/Views/AppBarCommands";
+import { Icons } from "PosApi/Create/Views";
+
+export default class ManageShiftsUpdCommand extends ManageShiftsView.ManageShiftsExtensionCommandBase {
+
+    constructor(context: IExtensionCommandContext<ManageShiftsView.IManageShiftsToExtensionCommandMessageTypeMap>) {
+        super(context);
+        this.label = "Numeric Dialog";
+        this.id = "NumericDialog";
+        this.extraClass = Icons.NumberSymbol;
+    }
+
+    protected init(state: ManageShiftsView.IManageShiftsExtensionCommandState): void {
+        this.isVisible = true;
+        this.canExecute = true;
+    }
+
+    protected execute(): void {
+        this.context.navigator.navigate("NumericInputDialogView");
+    }
+
+}
---
+++ POS_Samples/NumpadDialog/README.md
@@ -0,0 +1,2 @@
+
+## This sample is to show how to delete cart by CSU API
