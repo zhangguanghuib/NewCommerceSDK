16c3e6aac0b3f55450274a8f79d16846a64841e9
zhangguanghuib <=>
2023-06-05 21:51:48 +0800

    Disable save customer email address on receipt dialog

---
+++ POS_Samples/Disable.SaveEmailAdress.Customer/HardwareStation/ICoinDispenser.cs
@@ -0,0 +1,12 @@
+﻿namespace GHZ
+{
+    namespace HardwareStation.CoinDispenserSample
+    {
+        using Microsoft.Dynamics.Commerce.HardwareStation.Peripherals;
+
+        public interface ICoinDispenser : IPeripheral
+        {
+            void DispenseChange(int amout);
+        }
+    }
+}
---
+++ POS_Samples/Disable.SaveEmailAdress.Customer/Pos/Handlers/GetReceiptEmailAddressClientRequestHandlerExt.ts
@@ -0,0 +1,21 @@
+﻿import { GetReceiptEmailAddressClientRequestHandler } from "PosApi/Extend/RequestHandlers/CartRequestHandlers";
+import { GetReceiptEmailAddressClientRequest, GetReceiptEmailAddressClientResponse } from "PosApi/Consume/Cart";
+import { ClientEntities } from "PosApi/Entities";
+
+export default class GetReceiptEmailAddressClientRequestHandlerExt extends GetReceiptEmailAddressClientRequestHandler {
+
+    /**
+     * Executes the request handler asynchronously.
+     * @param {GetReceiptEmailAddressClientRequest<GetReceiptEmailAddressClientResponse>} The request containing the response.
+     * @return {Promise<ICancelableDataResult<GetReceiptEmailAddressClientResponse>>} The cancelable promise containing the response.
+     */
+    public executeAsync(request: GetReceiptEmailAddressClientRequest<GetReceiptEmailAddressClientResponse>):
+        Promise<ClientEntities.ICancelableDataResult<GetReceiptEmailAddressClientResponse>> {
+
+        if (Object.prototype.hasOwnProperty.call(request, "disableSaveEmailToCustomer")) {
+            request["disableSaveEmailToCustomer"] = true;
+        }
+
+        return this.defaultExecuteAsync(request);
+    }
+}
