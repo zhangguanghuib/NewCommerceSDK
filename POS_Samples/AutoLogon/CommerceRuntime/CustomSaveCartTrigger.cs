b8069809f10fdabfe4cfd7f476c3e641b392a1eb
zhangguanghuib <=>
2023-06-06 08:08:15 +0800

    Retrieve PWD  from Azure

---
+++ POS_Samples/AutoLogon/CommerceRuntime/CustomSaveCartTrigger.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Dynamics.Commerce.Runtime;
+using Microsoft.Dynamics.Commerce.Runtime.Messages;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contoso.GasStationSample.CommerceRuntime
+{
+    public class CustomSaveCartTrigger : IRequestTriggerAsync
+    {
+        public IEnumerable<Type> SupportedRequestTypes
+        {
+            get
+            {
+                return new[] { typeof(SaveCartRequest) };
+            }
+        }
+
+        public  Task OnExecuted(Request request, Response response)
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task OnExecuting(Request request)
+        {
+            ThrowIf.Null(request, "request");
+            Type requestedType = request.GetType();
+            string result = null;
+            if (requestedType == typeof(SaveCartRequest))
+            {
+                string staffId = request.RequestContext.GetPrincipal().StaffId;
+                GetUserDefinedSecretStringValueServiceRequest keyVaultRequest = new GetUserDefinedSecretStringValueServiceRequest(staffId);
+                GetUserDefinedSecretStringValueServiceResponse keyVaultResponse = await request.RequestContext.ExecuteAsync<GetUserDefinedSecretStringValueServiceResponse>(keyVaultRequest).ConfigureAwait(false);
+                result = keyVaultResponse.SecretStringValue;
+            }
+        }
+    }
+}
---
+++ POS_Samples/AutoLogon/CommerceRuntime/NonBindableOperationController.Employee.cs
@@ -0,0 +1,27 @@
+﻿namespace Contoso.GasStationSample.CommerceRuntime
+{
+    using Microsoft.Dynamics.Commerce.Runtime;
+    using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+    using Microsoft.Dynamics.Commerce.Runtime.DataServices.Messages;
+    using Microsoft.Dynamics.Commerce.Runtime.Hosting.Contracts;
+    using Microsoft.Dynamics.Commerce.Runtime.Messages;
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class NonBindableOperationController : IController
+    {
+        [HttpPost]
+        [Authorization(CommerceRoles.Anonymous)]
+        public async Task<string> RetrieveStaffPwd(IEndpointContext context, string staffId)
+        {
+            GetUserDefinedSecretStringValueServiceRequest keyVaultRequest = new GetUserDefinedSecretStringValueServiceRequest(staffId);
+            GetUserDefinedSecretStringValueServiceResponse keyVaultResponse =
+                await context.ExecuteAsync<GetUserDefinedSecretStringValueServiceResponse>(keyVaultRequest).ConfigureAwait(false);
+            
+            return keyVaultResponse.SecretStringValue;
+        }
+
+    }
+}
