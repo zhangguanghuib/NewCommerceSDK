b660594843c8cf94eb7944db7821ee7ef98237fe
zhangguanghuib <=>
2023-07-18 17:35:28 +0800

    Extension  properties

---
+++ POS_Samples/ExtensionPropertiesSample/HardwareStation/CustomPingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GHZ
+{
+    namespace HardwareStation.CoinDispenserSample
+    {
+        using System.Threading.Tasks;
+        using Microsoft.Dynamics.Commerce.Runtime;
+        using Microsoft.Dynamics.Commerce.Runtime.Hosting.Contracts;
+        using GHZ.HardwareStation.CoinDispenserSample.Messages;
+
+        [RoutePrefix("CUSTOMPING")]
+        public class CustomPingController: IController
+        {
+            [HttpPost]
+            public Task<string> CustomPing(PingRequest pingRequest)
+            {
+                ThrowIf.Null(pingRequest, "pingRequest");
+
+                return Task.FromResult(string.Format("Your message is successfully received: {0}", pingRequest.Message));
+            }
+        }
+    }
+}
