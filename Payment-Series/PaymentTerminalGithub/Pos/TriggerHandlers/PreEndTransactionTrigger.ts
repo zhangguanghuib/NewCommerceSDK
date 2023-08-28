45c87ce851b89cd6fc7bcccf385ffde846036dec
zhangguanghuib <=>
2023-08-28 21:33:22 +0800

    PaymentDevice for test

---
+++ Payment-Series/PaymentTerminalGithub/Pos/TriggerHandlers/PreEndTransactionTrigger.ts
@@ -0,0 +1,27 @@
+﻿import * as Triggers from "PosApi/Extend/Triggers/TransactionTriggers";
+import { ClientEntities, ProxyEntities } from "PosApi/Entities";
+import { ObjectExtensions } from "PosApi/TypeExtensions";
+import {
+    GetCurrentCartClientRequest, GetCurrentCartClientResponse
+} from "PosApi/Consume/Cart";
+
+export default class PreEndTransactionTrigger extends Triggers.PreEndTransactionTrigger {
+
+    public execute(options: Triggers.IPreEndTransactionTriggerOptions): Promise<ClientEntities.ICancelable> {
+        console.log("Executing PreEndTransactionTrigger with options " + JSON.stringify(options) + ".");
+
+        let currentCart: ProxyEntities.Cart;
+        return this.context.runtime.executeAsync<GetCurrentCartClientResponse>(new GetCurrentCartClientRequest())
+            .then((getCurrentCartClientResponse: ClientEntities.ICancelableDataResult<GetCurrentCartClientResponse>):
+                Promise<ClientEntities.ICancelable> => {
+                currentCart = getCurrentCartClientResponse.data.result;
+
+                // If the current cart is not empty but without CustomerId
+                if (!ObjectExtensions.isNullOrUndefined(currentCart) && ObjectExtensions.isNullOrUndefined(currentCart.CustomerId)) {
+                    return Promise.reject(new ClientEntities.ExtensionError("Not allow to checkout without customer"));
+                } else {
+                    return Promise.resolve({ canceled: false, data: null });
+                }
+            });
+    }
+}
