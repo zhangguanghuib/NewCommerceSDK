28703ead4ee70c756762b9bf3e962ba18b2e3d04
zhangguanghuib <=>
2023-10-22 22:48:57 +0800

    reason code

---
+++ POS_Samples/ReturnTransaction.Restriction/CommerceRuntime/GetCustomReceiptFieldsService.cs
@@ -0,0 +1,80 @@
+﻿namespace Contoso
+{
+    namespace Commerce.Runtime.ReceiptsSample
+    {
+        using System;
+        using System.Collections.Generic;
+        using System.Linq;
+        using System.Threading.Tasks;
+        using Microsoft.Dynamics.Commerce.Runtime;
+        using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+        using Microsoft.Dynamics.Commerce.Runtime.DataServices.Messages;
+        using Microsoft.Dynamics.Commerce.Runtime.Messages;
+        using Microsoft.Dynamics.Commerce.Runtime.Services.Messages;
+
+        public class GetCustomReceiptFieldsService : IRequestHandlerAsync
+        {
+            /// <summary>
+            /// Gets the supported request types.
+            /// </summary>
+            public IEnumerable<Type> SupportedRequestTypes
+            {
+                get
+                {
+                    return new[]
+                    {
+                        typeof(GetSalesTransactionCustomReceiptFieldServiceRequest),
+                    };
+                }
+            }
+
+            /// <summary>
+            /// Executes the requests.
+            /// </summary>
+            /// <param name="request">The request parameter.</param>
+            /// <returns>The GetReceiptServiceResponse that contains the formatted receipts.</returns>
+            public async Task<Response> Execute(Request request)
+            {
+                if (request == null)
+                {
+                    throw new ArgumentNullException("request");
+                }
+
+                Type requestedType = request.GetType();
+
+                if (requestedType == typeof(GetSalesTransactionCustomReceiptFieldServiceRequest))
+                {
+                    return await this.GetCustomReceiptFieldForSalesTransactionReceiptsAsync((GetSalesTransactionCustomReceiptFieldServiceRequest)request).ConfigureAwait(false);
+                }
+
+                throw new NotSupportedException(string.Format("Request '{0}' is not supported.", request.GetType()));
+            }
+
+            /// <summary>
+            /// Gets the custom receipt filed for all transaction-based receipts, such as SalesReceipt, CustomerOrderReceipt, PickupReceipt, CreditCardReceipt, and so on.
+            /// </summary>
+            /// <param name="request">The service request to get custom receipt filed.</param>
+            /// <returns>The value of custom receipt filed.</returns>
+            private Response GetCustomReceiptFieldForSalesTransactionReceiptsAsync(GetSalesTransactionCustomReceiptFieldServiceRequest request)
+            {
+                string receiptFieldName = request.CustomReceiptField;
+
+                SalesLine salesLine = request.SalesLine;
+
+                string returnValue = null;
+                switch (receiptFieldName)
+                {
+                    case "ITEMNUMBER":
+                        {
+                            var reasonCodes  = salesLine.ReasonCodeLines.Select(x => x.Information).ToArray();
+                            returnValue = String.Join("", reasonCodes);
+                        }
+
+                        break;
+                }
+
+                return new GetCustomReceiptFieldServiceResponse(returnValue);
+            }
+        }
+    }
+}
---
+++ POS_Samples/ReturnTransaction.Restriction/Pos/TriggerHandlers/PostProductSaleTrigger.ts
@@ -0,0 +1,29 @@
+﻿import { GetReasonCodeLinesClientRequest, GetReasonCodeLinesClientResponse, SaveReasonCodeLinesOnCartLinesClientRequest, SaveReasonCodeLinesOnCartLinesClientResponse } from "PosApi/Consume/Cart";
+import { ClientEntities, ProxyEntities } from "PosApi/Entities";
+import * as Triggers from "PosApi/Extend/Triggers/ProductTriggers";
+
+export default class PostProductSaleTrigger extends Triggers.PostProductSaleTrigger {
+
+    public execute(options: Triggers.IPostProductSaleTriggerOptions): Promise<void> {
+
+        let correlationId: string = this.context.logger.getNewCorrelationId();
+
+        let getReasonCodeLinesClientRequest: GetReasonCodeLinesClientRequest<GetReasonCodeLinesClientResponse>
+            = new GetReasonCodeLinesClientRequest(['IMEA'], correlationId);
+
+        return this.context.runtime.executeAsync(getReasonCodeLinesClientRequest)
+            .then((response: ClientEntities.ICancelableDataResult<GetReasonCodeLinesClientResponse>): Promise<ClientEntities.ICancelableDataResult<SaveReasonCodeLinesOnCartLinesClientResponse>> => {
+                let reasonCodeLines: ProxyEntities.ReasonCodeLine[] = response.data.result;
+                let reasonCodeLinesOnCartLine: ClientEntities.IReasonCodeLinesOnCartLine =  {
+                    cartLineId: options.cart.CartLines[0].LineId,
+                    reasonCodeLines: options.cart.CartLines[0].ReasonCodeLines.concat(reasonCodeLines)
+                };
+
+                let saveReasonCodeLinesOnCartLinesClientRequest: SaveReasonCodeLinesOnCartLinesClientRequest<SaveReasonCodeLinesOnCartLinesClientResponse>
+                    = new SaveReasonCodeLinesOnCartLinesClientRequest([reasonCodeLinesOnCartLine], correlationId);
+                return this.context.runtime.executeAsync(saveReasonCodeLinesOnCartLinesClientRequest);
+            }).then((response: ClientEntities.ICancelableDataResult<SaveReasonCodeLinesOnCartLinesClientResponse>): Promise<void> => {
+                return Promise.resolve();
+            });
+    }
+}
