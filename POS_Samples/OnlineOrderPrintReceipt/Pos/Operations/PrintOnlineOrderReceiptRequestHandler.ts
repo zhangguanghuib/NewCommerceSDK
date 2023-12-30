b808b2402f192168650903f1d4d8d88b4d018d1e
zhangguanghuib <=>
2023-12-30 21:09:21 +0800

    timer debounce

--- POS_Samples/OnlineOrderPrintReceipt/Pos/Operations/PrintOnlineOrderReceiptRequestHandler.ts
+++ POS_Samples/OnlineOrderPrintReceipt/Pos/Operations/PrintOnlineOrderReceiptRequestHandler.ts
@@ -7,15 +7,26 @@ import PrintOnlineOrderReceiptResponse from "./PrintOnlineOrderReceiptResponse";
 export default class PrintOnlineOrderReceiptRequestHandler<TResponse extends PrintOnlineOrderReceiptResponse>
     extends ExtensionOperationRequestHandlerBase<TResponse> {
 
+    private timerId: number = null;
+    private PrintOnlineOrderReceiptTimerId: string = 'PrintOnlineOrderReceiptTimerId';
+
     public executeAsync(request: PrintOnlineOrderReceiptRequest<TResponse>): Promise<ClientEntities.ICancelableDataResult<TResponse>> {
 
         let onlineOrderReceiptService: OnlineOrderReceiptService = new OnlineOrderReceiptService(this.context);
 
         let response: PrintOnlineOrderReceiptResponse = new PrintOnlineOrderReceiptResponse();
 
-        setInterval(async (): Promise<void> => {
+        //Debounce
+        if (localStorage.getItem(this.PrintOnlineOrderReceiptTimerId)) {
+            this.timerId = Number(localStorage.getItem(this.PrintOnlineOrderReceiptTimerId));
+            clearInterval(this.timerId);
+        }
+
+        this.timerId = setInterval(async (): Promise<void> => {
             await onlineOrderReceiptService.processByAsyncAwait();
         }, 5000);
+
+        localStorage.setItem(this.PrintOnlineOrderReceiptTimerId, this.timerId.toString());
 
         return Promise.resolve().then((): ClientEntities.ICancelableDataResult<TResponse> => {
             return <ClientEntities.ICancelableDataResult<TResponse>>{
