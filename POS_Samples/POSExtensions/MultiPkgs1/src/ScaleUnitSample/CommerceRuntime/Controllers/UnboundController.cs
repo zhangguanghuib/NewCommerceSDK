45500324821e79236406658fc370f2a62bc8e752
zhangguanghuib <=>
2024-03-06 12:08:17 +0800

    Multipkgs

---
+++ POS_Samples/POSExtensions/MultiPkgs1/src/ScaleUnitSample/CommerceRuntime/Controllers/UnboundController.cs
@@ -0,0 +1,44 @@
+namespace Contoso.CommerceRuntime.Controllers
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Threading.Tasks;
+    using Azure.Core;
+    using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+    using Microsoft.Dynamics.Commerce.Runtime.DataServices.Messages;
+    using Microsoft.Dynamics.Commerce.Runtime.Hosting.Contracts;
+    using Microsoft.Dynamics.Commerce.Runtime.Messages;
+    using SO=Contoso.Commerce.Runtime.StoreHoursSample.Messages;
+
+
+    /// <summary>
+    /// An extension controller to handle requests to the StoreHours entity set.
+    /// </summary>
+    public class UnboundController : IController
+    {
+        /// <summary>
+        /// A simple GET endpoint to demonstrate GET endpoints on an unbound controller.
+        /// </summary>
+        /// <returns>A simple true value to indicate the endpoint was reached.</returns>
+        [HttpGet]
+        [Authorization(CommerceRoles.Anonymous, CommerceRoles.Application, CommerceRoles.Customer, CommerceRoles.Device, CommerceRoles.Employee, CommerceRoles.Storefront)]
+        public async Task<bool> SimplePingGet(IEndpointContext context)
+        {
+            // Call request from another package
+            var getStoreHoursDataRequest = new SO.GetStoreHoursDataRequest("HOUSTON") { QueryResultSettings = QueryResultSettings.AllRecords };
+            var hoursResponse = await context.ExecuteAsync<SO.GetStoreHoursDataResponse>(getStoreHoursDataRequest).ConfigureAwait(false);
+            return true;
+        }
+
+        /// <summary>
+        /// A simple POST endpoint to demonstrate POST endpoints on an unbound controller.
+        /// </summary>
+        /// <returns>A simple true value to indicate the endpoint was reached.</returns>
+        [HttpPost]
+        [Authorization(CommerceRoles.Customer, CommerceRoles.Device, CommerceRoles.Employee)]
+        public Task<bool> SimplePingPost(IEndpointContext context)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ POS_Samples/POSExtensions/MultiPkgs1/src/ScaleUnitSample/DataModelStoreHours/Entities/StoreDayHours.cs
@@ -0,0 +1,87 @@
+﻿namespace Contoso
+{
+    namespace Commerce.Runtime.DataModel
+    {
+        using System.Runtime.Serialization;
+        using Microsoft.Dynamics.Commerce.Runtime.ComponentModel.DataAnnotations;
+        using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+        using SystemDataAnnotations = System.ComponentModel.DataAnnotations;
+
+        /// <summary>
+        /// Defines a simple class that holds information about opening and closing times for a particular day.
+        /// </summary>
+        public class StoreDayHours : CommerceEntity
+        {
+            private const string DayColumn = "DAY";
+            private const string OpenTimeColumn = "OPENTIME";
+            private const string CloseTimeColumn = "CLOSINGTIME";
+            private const string IdColumn = "RECID";
+            private const string StoreNumberColumn = "STORENUMBER";
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="StoreDayHours"/> class.
+            /// </summary>
+            public StoreDayHours()
+                : base("StoreDayHours")
+            {
+            }
+
+            /// <summary>
+            /// Gets or sets the day of the week.
+            /// </summary>
+            [DataMember]
+            [Column(DayColumn)]
+            public int DayOfWeek
+            {
+                get { return (int)this[DayColumn]; }
+                set { this[DayColumn] = value; }
+            }
+
+            /// <summary>
+            /// Gets or sets the open time.
+            /// </summary>
+            [DataMember]
+            [Column(OpenTimeColumn)]
+            public int OpenTime
+            {
+                get { return (int)this[OpenTimeColumn]; }
+                set { this[OpenTimeColumn] = value; }
+            }
+
+            /// <summary>
+            /// Gets or sets the closing time.
+            /// </summary>
+            [DataMember]
+            [Column(CloseTimeColumn)]
+            public int CloseTime
+            {
+                get { return (int)this[CloseTimeColumn]; }
+                set { this[CloseTimeColumn] = value; }
+            }
+
+            /// <summary>
+            /// Gets or sets the id.
+            /// </summary>
+            [SystemDataAnnotations.Key]
+            [Key]
+            [DataMember]
+            [Column(IdColumn)]
+            public long Id
+            {
+                get { return (long)this[IdColumn]; }
+                set { this[IdColumn] = value; }
+            }
+
+            /// <summary>
+            /// Gets or sets the channel id.
+            /// </summary>
+            [DataMember]
+            [Column(StoreNumberColumn)]
+            public string ChannelId
+            {
+                get { return (string)this[StoreNumberColumn]; }
+                set { this[StoreNumberColumn] = value; }
+            }
+        }
+    }
+}
