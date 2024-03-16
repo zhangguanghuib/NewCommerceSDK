620451a0aea4b20244f14317414052e934c4c9b0
zhangguanghuib <=>
2024-03-16 21:14:21 +0800

    Remove some section from customer details form

---
+++ POS_Samples/Solutions/SampleExtensions/Pos/TriggerHandlers/PreSearchOrdersTrigger.ts
@@ -0,0 +1,42 @@
+﻿import { StringExtensions, ObjectExtensions } from "PosApi/TypeExtensions";
+import { ProxyEntities } from "PosApi/Entities";
+import { CancelableTriggerResult } from "PosApi/Extend/Triggers/Triggers";
+import MessageDialog from "../Controls/DialogSample/MessageDialog";
+import * as Triggers from "PosApi/Extend/Triggers/SalesOrderTriggers";
+import IPreElevateUserTriggerOptions = Triggers.IPreSearchOrdersTriggerOptions;
+
+/**
+ * Example implementation of an PreSearchOrdersTrigger trigger that modifies the order search criteria and logs to the console.
+ */
+export default class PreSearchOrdersTrigger extends Triggers.PreSearchOrdersTrigger {
+    /**
+     * Executes the trigger functionality.
+     * The orderSearchCriteria can be updated on the newOptions and this becomes the default value in the orders search view.
+     * @param {Triggers.IPreUnlockTerminalTriggerOptions} options The options provided to the trigger.
+     */
+    public execute(options: IPreElevateUserTriggerOptions): Promise<CancelableTriggerResult<IPreElevateUserTriggerOptions>> {
+        const NEW_ORDER_SEARCH_CRITERIA: ProxyEntities.OrderSearchCriteria = {
+            CustomerAccountNumber: "testCustomerAccountNumber",
+            CustomerName: "testCustomerName",
+            EmailAddress: "testEmailAddress",
+            SalesId: "testSalesId",
+            ReceiptId: "testReceiptId",
+            OrderStatusValues: [1, 2, 3],
+            StartDateTime: new Date(),
+            EndDateTime: new Date(),
+            ChannelReferenceId: "testChannelReferenceId",
+            StoreId: "testStoreId",
+            OrderType: ProxyEntities.CustomerOrderType.SalesOrder,
+            CustomFilters: undefined
+        };
+        let newOptions: IPreElevateUserTriggerOptions = ObjectExtensions.clone(options);
+
+        newOptions.orderSearchCriteria = NEW_ORDER_SEARCH_CRITERIA;
+
+        return MessageDialog.show(this.context, StringExtensions.format("Executing PreSearchOrdersTrigger with options {0}.", JSON.stringify(newOptions)))
+            .then((): Promise<CancelableTriggerResult<IPreElevateUserTriggerOptions>> => {
+                // Returning a CancelableTriggerResult is needed for the data modification in the options to take effect.
+                return Promise.resolve(new CancelableTriggerResult(false, newOptions));
+            });
+    }
+}
---
+++ POS_Samples/Solutions/SampleExtensions/Pos/ViewExtensions/CustomerDetails/CustomerCrossLoyaltyCommand.ts
@@ -0,0 +1,43 @@
+﻿import { IExtensionCommandContext } from "PosApi/Extend/Views/AppBarCommands";
+import * as CustomerDetailsView from "PosApi/Extend/Views/CustomerDetailsView";
+
+export default class CustomerCrossLoyaltyCommand extends CustomerDetailsView.CustomerDetailsExtensionCommandBase {
+    /**
+     * Creates a new instance of the CustomerCrossLoyaltyCommand class.
+     * @param {IExtensionCommandContext<CustomerDetailsView.ICustomerDetailsToExtensionCommandMessageTypeMap>} context The command context.
+     * @remarks The command context contains APIs through which a command can communicate with POS.
+     */
+    constructor(context: IExtensionCommandContext<CustomerDetailsView.ICustomerDetailsToExtensionCommandMessageTypeMap>) {
+        super(context);
+
+        this.id = "customerCrossLoyaltyCommand";
+        this.label = "Cross Loyalty Discount";
+        this.extraClass = "iconLightningBolt";
+    }
+
+    /**
+     * Initializes the command.
+     * @param {CustomerDetailsView.ICustomerDetailsExtensionCommandState} state The state used to initialize the command.
+     */
+    protected init(state: CustomerDetailsView.ICustomerDetailsExtensionCommandState): void {
+
+        this.isVisible = false;
+        this.canExecute = true;
+
+        let loyaltyCardListDiv: HTMLDivElement = document.querySelector("#loyaltyCardList") as HTMLDivElement;
+        loyaltyCardListDiv?.parentElement?.parentElement?.remove();
+
+        let wishListDiv: HTMLDivElement = document.querySelector("#wishList") as HTMLDivElement;
+        wishListDiv?.parentElement?.parentElement?.remove();
+
+        let customerRecommendedProductsListDiv: HTMLDivElement = document.querySelector("#customerRecommendedProductsList") as HTMLDivElement;
+        customerRecommendedProductsListDiv?.parentElement?.parentElement?.remove();
+    }
+
+    /**
+     * Executes the command.
+     */
+    protected execute(): void {
+
+    }
+}
