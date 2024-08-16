f2afd908e0c200ec687af8913b76202dba338c34
Guanghui Zhang <zhangguanghuib@126.com>
2024-08-16 16:06:39 +0800

    healthcheck

---
+++ Readme_Docs/7. Health Check by PowerShell/CSUHealthCheck.ps1
@@ -0,0 +1,22 @@
+﻿$computerName = $env:COMPUTERNAME
+$port = "446"
+$baseUrl = "https://$($computerName):$($port)/RetailServer"
+$healthcheckEndpoint = "/healthcheck?testname=ping"
+$ServerEndPoint = "/Commerce"
+$healthCheckUrl = $baseUrl + $healthcheckEndpoint 
+$ServerUrl =  $baseUrl + $ServerEndPoint 
+
+ Write-Output "$($baseUrl)"
+ Write-Output "$($healthCheckUrl)"
+
+try {
+    $response = Invoke-WebRequest -Uri $healthCheckUrl -UseBasicParsing
+
+    if ($response.StatusCode -eq 200) {
+        Write-Output "$($ServerUrl) is running properly. Status code: $($response.StatusCode)"
+    } else {
+        Write-Output "$($ServerUrl) responded, but with an unexpected status code: $($response.StatusCode)"
+    }
+} catch {
+    Write-Output "Failed to reach the server. Error: $_"
+}
---
+++ Readme_Docs/7. Health Check by PowerShell/DeployThenHealthCheck.ps1
@@ -0,0 +1,11 @@
+﻿# Path to the first script
+$deployScript = ".\deployCSU.ps1"
+
+# Path to the second script
+$healthCheckScript = ".\CSUHealthCheck.ps1"
+
+# Execute the first script
+#& $deployScript
+
+# Execute the second script after the first one completes
+& $healthCheckScript
