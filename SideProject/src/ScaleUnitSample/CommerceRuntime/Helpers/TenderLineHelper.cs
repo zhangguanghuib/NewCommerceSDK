fa8e8fd00fc67c6c44372a85e815cefb65420721
Guanghui Zhang <zhangguanghuib@126.com>
2024-09-05 15:12:00 +0800

    Some testing

---
+++ SideProject/src/ScaleUnitSample/CommerceRuntime/Helpers/TenderLineHelper.cs
@@ -0,0 +1,25 @@
+﻿
+namespace CommerceRuntime.Helpers
+{
+    using Microsoft.Dynamics.Commerce.Runtime.DataModel;
+    using Microsoft.Dynamics.Commerce.Runtime;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class TenderLineHelper
+    {
+        public static void FillTenderLinesNumber(IList<TenderLine> tenderLines)
+        {
+            if (tenderLines.IsNullOrEmpty())
+            {
+                return;
+            }
+            decimal num = tenderLines.Max((TenderLine l) => l.LineNumber) + 1m;
+            foreach (TenderLine item in tenderLines.Where((TenderLine x) => x.Status != TenderLineStatus.Historical && x.LineNumber == 0m))
+            {
+                item.LineNumber = num++;
+            }
+        }
+    }
+}
---
+++ SideProject/src/ScaleUnitSample/CommerceRuntime/InboundOutboundTriggers/InboundOutboundTrigger.cs
@@ -0,0 +1,63 @@
+﻿
+namespace CommerceRuntime.InboundOutboundTriggers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Request = Microsoft.Dynamics.Commerce.Runtime.Messages.Request;
+    using Response = Microsoft.Dynamics.Commerce.Runtime.Messages.Response;
+    using Microsoft.Dynamics.Commerce.Runtime;
+    using Microsoft.Dynamics.Commerce.Runtime.Services.Messages;
+
+    public class InboundOutboundTrigger : IRequestTriggerAsync
+    {
+        public IEnumerable<Type> SupportedRequestTypes
+        {
+            get
+            {
+                return new[] { typeof(AddInventoryInboundOutboundDocumentLineRequest) };
+            }
+        }
+
+        public Task OnExecuted(Request request, Response response)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnExecuting(Request request)
+        {
+            var req = request as AddInventoryInboundOutboundDocumentLineRequest;
+            if (req.Line.QuantityToUpdate > 999999)
+            {
+                req.Line.QuantityToUpdate = 0m;
+                throw new CommerceException("InventoryInboundOutboundDocumentLine", "[Inbound Outbound] Quantity can not greater than 999999");
+            }
+            return Task.CompletedTask;
+        }
+    }
+
+    public class MKInventoryInboundOutboundDocumentUpdateLineTrigger : IRequestTriggerAsync
+    {
+        public IEnumerable<Type> SupportedRequestTypes
+        {
+            get
+            {
+                return new[] { typeof(UpdateInventoryInboundOutboundDocumentLineRequest) };
+            }
+        }
+        public Task OnExecuted(Request request, Response response)
+        {
+            return Task.CompletedTask;
+        }
+        public Task OnExecuting(Request request)
+        {
+            var req = request as UpdateInventoryInboundOutboundDocumentLineRequest;
+            if (req.Line.QuantityToUpdate > 999999)
+            {
+                req.Line.QuantityToUpdate = 0;
+                throw new CommerceException("InventoryInboundOutboundDocumentLine", "[Inbound Outbound] Quantity can not greater than 999999");
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
