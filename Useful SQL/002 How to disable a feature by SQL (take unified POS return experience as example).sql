4bd20b20940cb4f5c22310aa35629754ddf4a91c
Guanghui Zhang <zhangguanghuib@126.com>
2024-09-05 16:48:14 +0800

    Update 002 How to disable a feature by SQL (take unified POS return experience as example).sql

--- Useful SQL/002 How to disable a feature by SQL (take unified POS return experience as example).sql
+++ Useful SQL/002 How to disable a feature by SQL (take unified POS return experience as example).sql
@@ -1,4 +1,6 @@
-DECLARE @featureClassNamePattern NVARCHAR(100) = '%Dynamics.AX.Application.RetailDeliveryModeConsistencyFeature%';
+--DECLARE @featureClassNamePattern NVARCHAR(100) = '%Dynamics.AX.Application.RetailDeliveryModeConsistencyFeature%';
+--Dynamics.AX.Application.RetailUnifiedReturnUXImprovementFeature
+DECLARE @featureClassNamePattern NVARCHAR(100) = 'Dynamics.AX.Application.RetailUnifiedReturnsFeature';
 DECLARE @featureStateRecId BIGINT;
 
 SELECT @featureStateRecId = recid FROM dbo.featuremanagementstate WHERE NAME LIKE @featureClassNamePattern
@@ -9,7 +11,7 @@ SELECT enabledate, modifieddatetime, modifiedby, * FROM dbo.featuremanagementmet
 UPDATE [dbo].[featuremanagementmetadata]
 SET    enabledate = '1900-01-01 00:00:00.000',
        modifieddatetime = Getdate(),
-       modifiedby = <alias\_incidentnumber>
+       modifiedby = 'Testing'
 WHERE  featurestate = @featureStateRecId
 
 UPDATE [dbo].featuremanagementstate
@@ -18,3 +20,7 @@ WHERE  recid = @featureStateRecId
 
 SELECT * FROM dbo.featuremanagementstate WHERE recid = @featureStateRecId
 SELECT enabledate, modifieddatetime, modifiedby, * FROM dbo.featuremanagementmetadata WHERE  featurestate = @featureStateRecId
+
+
+
+SELECT * FROM dbo.featuremanagementstate WHERE NAME LIKE 'Dynamics.AX.Application.RetailUnifiedReturnsFeature'
